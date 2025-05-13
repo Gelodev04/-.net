751b7957500274a1b116fa69f6f79e1649aa8120
gelo <gelodev83@gmail.com>
2025-05-13 11:44:33 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -48,9 +48,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-// RICH EDITOR
+// // RICH EDITOR
 //  ClassicEditor
 //     .create(document.querySelector('#editor'), {
+//       htmlSupport: {
+//       allow: [
+//         {
+//           name: 'div',
+//           attributes: ['contenteditable', 'class'],
+//           classes: true,
+//           styles: true
+//         }
+//       ]
+//     },
 //       toolbar: [
 //         'heading',
 //         '|',
@@ -71,46 +81,269 @@ document.addEventListener("DOMContentLoaded", () => {
 //       console.error(error);
 //     });
 
+/**
+ * This configuration was generated using the CKEditor 5 Builder. You can modify it anytime using this link:
+ * https://ckeditor.com/ckeditor-5/builder/#installation/NoNgNARATAdArDADBSUDMAOKIAscQZwCMaAnGTmnHOkQOw4alx1FzmN1REZEgoQApgDsUiMMCJhx4qbIC6kIgEM6GOqToR5QA===
+ */
 
 const {
-     ClassicEditor,
-    Essentials,
-    Bold,
-    Italic,
-    Font,
-    Paragraph,
-    Heading,
-    ImageUpload,
-    MediaEmbed,
-    Link,
-    Alignment,
-    List,
-    BlockQuote,
-    Table,
-    Undo,
-    Redo,
-    SourceEditing,
-    GeneralHtmlSupport
-} = CKEDITOR;
+	ClassicEditor,
+	Alignment,
+	AutoImage,
+	Autosave,
+	BlockQuote,
+	Bold,
+	CKBox,
+	CKBoxImageEdit,
+	CloudServices,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	FullPage,
+	GeneralHtmlSupport,
+	Heading,
+	HtmlComment,
+	HtmlEmbed,
+	ImageBlock,
+	ImageCaption,
+	ImageEditing,
+	ImageInline,
+	ImageInsert,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	ImageUpload,
+	ImageUtils,
+	Italic,
+	Link,
+	LinkImage,
+	List,
+	ListProperties,
+	Paragraph,
+	PictureEditing,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	Underline
+} = window.CKEDITOR;
+const { SourceEditingEnhanced } = window.CKEDITOR_PREMIUM_FEATURES;
 
-ClassicEditor
-    .create( document.querySelector( '#editor' ), {
-        licenseKey: '<YOUR_LICENSE_KEY>',
-        plugins: [ Essentials, Bold, Italic, Font, Paragraph, Heading ],
-        toolbar: [
-        'heading',
-        '|',
-        'imageUpload', 'mediaEmbed', 'link',
-        '|',
-        'bold', 'italic',
-         'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
-        'alignment',
-        'bulletedList', 'numberedList',
-        '|',
-        'blockQuote', 'insertTable',
-        'undo', 'redo',
-        'sourceEditing'
-      ]
-    } )
-    .then( /* ... */ )
-    .catch( /* ... */ );
+const LICENSE_KEY =
+	'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDgzOTAzOTksImp0aSI6Ijk4OGU0YjA1LTNjZTEtNDRmYy1iMjY0LTAzNGRkMGNjOTk4MSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjVjYjZmNzJlIn0.DIrbDjnAMk9bf8ilkp-CayziHTAzbCDa9yk190_V1MXSUxOLgmqXAdY5TNBL9z3K7ToC3pkCPGGlaMhWHPmsJw';
+
+const CLOUD_SERVICES_TOKEN_URL =
+	'https://8o00lebiaabv.cke-cs.com/token/dev/75c211d0621d5f65d42103055aea396630bc92f96622a10a8f0889eef97c?limit=10';
+
+const editorConfig = {
+	toolbar: {
+		items: [
+		
+			'|',
+			'heading',
+			'|',
+			'insertImage',
+			'link',
+      '|',
+			'bold',
+			'italic',
+			'fontColor',
+			'fontBackgroundColor',
+			'alignment',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'blockQuote',
+			'insertTable',
+			'|',
+      	'sourceEditingEnhanced'
+
+		],
+		shouldNotGroupWhenFull: false
+	},
+	plugins: [
+		Alignment,
+		AutoImage,
+		Autosave,
+		BlockQuote,
+		Bold,
+		CKBox,
+		CKBoxImageEdit,
+		CloudServices,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		FullPage,
+		GeneralHtmlSupport,
+		Heading,
+		HtmlComment,
+		HtmlEmbed,
+		ImageBlock,
+		ImageCaption,
+		ImageEditing,
+		ImageInline,
+		ImageInsert,
+		ImageInsertViaUrl,
+		ImageResize,
+		ImageStyle,
+		ImageTextAlternative,
+		ImageToolbar,
+		ImageUpload,
+		ImageUtils,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		ListProperties,
+		Paragraph,
+		PictureEditing,
+		SourceEditingEnhanced,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
+		TableToolbar,
+		Underline
+	],
+	cloudServices: {
+		tokenUrl: CLOUD_SERVICES_TOKEN_URL
+	},
+	fontFamily: {
+		supportAllValues: true
+	},
+	fontSize: {
+		options: [10, 12, 14, 'default', 18, 20, 22],
+		supportAllValues: true
+	},
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: '문단',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: '문단 1',
+				class: 'ck-heading_heading1'
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: '문단 2',
+				class: 'ck-heading_heading2'
+			},
+		]
+	},
+	htmlSupport: {
+		allow: [
+			{
+				name: /^.*$/,
+				styles: true,
+				attributes: true,
+				classes: true
+			}
+		]
+	},
+	image: {
+		toolbar: [
+			'toggleImageCaption',
+			'imageTextAlternative',
+			'|',
+			'imageStyle:inline',
+			'imageStyle:wrapText',
+			'imageStyle:breakText',
+			'|',
+			'resizeImage',
+			'|',
+			'ckboxImageEdit'
+		]
+	},
+		
+	licenseKey: LICENSE_KEY,
+	link: {
+		addTargetToExternalLinks: true,
+		defaultProtocol: 'https://',
+		decorators: {
+			toggleDownloadable: {
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'file'
+				}
+			}
+		}
+	},
+	list: {
+		properties: {
+			styles: true,
+			startIndex: true,
+			reversed: true
+		}
+	},
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+	}
+};
+
+configUpdateAlert(editorConfig);
+
+ClassicEditor.create(document.querySelector('#editor'), editorConfig);
+
+/**
+ * This function exists to remind you to update the config needed for premium features.
+ * The function can be safely removed. Make sure to also remove call to this function when doing so.
+ */
+function configUpdateAlert(config) {
+	if (configUpdateAlert.configUpdateAlertShown) {
+		return;
+	}
+
+	const isModifiedByUser = (currentValue, forbiddenValue) => {
+		if (currentValue === forbiddenValue) {
+			return false;
+		}
+
+		if (currentValue === undefined) {
+			return false;
+		}
+
+		return true;
+	};
+
+	const valuesToUpdate = [];
+
+	configUpdateAlert.configUpdateAlertShown = true;
+
+	if (!isModifiedByUser(config.licenseKey, '<YOUR_LICENSE_KEY>')) {
+		valuesToUpdate.push('LICENSE_KEY');
+	}
+
+	if (!isModifiedByUser(config.cloudServices?.tokenUrl, '<YOUR_CLOUD_SERVICES_TOKEN_URL>')) {
+		valuesToUpdate.push('CLOUD_SERVICES_TOKEN_URL');
+	}
+
+	if (valuesToUpdate.length) {
+		window.alert(
+			[
+				'Please update the following values in your editor config',
+				'to receive full access to Premium Features:',
+				'',
+				...valuesToUpdate.map(value => ` - ${value}`)
+			].join('\n')
+		);
+	}
+}
+
+
+
